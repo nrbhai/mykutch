2a1f867c0f36c066fd0b853a9b97c220c0945bc6
nrbhai <rachhnn@gmail.com>
2026-03-21 16:30:56 +0530

    Update sitemap generation to be dynamic and generate new sitemap

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Generate sitemap.xml for MyKutch website
+Generate sitemap.xml for MyKutch website dynamically
 """
 
 import os
@@ -15,72 +15,55 @@ def generate_sitemap():
     # Current date for lastmod
     current_date = datetime.now().strftime('%Y-%m-%d')
     
-    # Define pages with their priorities and change frequencies
-    pages = [
-        # Homepage - highest priority
-        ('', 1.0, 'weekly'),
-        
-        # Main pages
-        ('about.html', 0.8, 'monthly'),
-        ('blog.html', 0.8, 'weekly'),
-        ('bookings.html', 0.9, 'weekly'),
-        ('crafts.html', 0.8, 'weekly'),
-        ('destinations.html', 0.9, 'weekly'),
-        ('distance-matrix.html', 0.7, 'monthly'),
-        ('geography.html', 0.7, 'monthly'),
-        ('hidden-gems.html', 0.8, 'weekly'),
-        ('history.html', 0.7, 'monthly'),
-        ('landscapes.html', 0.7, 'weekly'),
-        
-        # Craft pages
-        ('crafts/ajrakh.html', 0.7, 'monthly'),
-        ('crafts/bandhani.html', 0.7, 'monthly'),
-        ('crafts/leather-craft.html', 0.7, 'monthly'),
-        ('crafts/mirror-work.html', 0.7, 'monthly'),
-        ('crafts/pottery.html', 0.7, 'monthly'),
-        ('crafts/rogan-art.html', 0.7, 'monthly'),
-        ('crafts/sudi-chappu.html', 0.7, 'monthly'),
-        ('crafts/weaving.html', 0.7, 'monthly'),
-        
-        # Destination pages - high priority
-        ('destinations/72-jinalaya.html', 0.6, 'monthly'),
-        ('destinations/abjibapa-chatardi.html', 0.6, 'monthly'),
-        ('destinations/adipur.html', 0.6, 'monthly'),
-        ('destinations/anjar.html', 0.6, 'monthly'),
-        ('destinations/asar-mata-beach.html', 0.6, 'monthly'),
-        ('destinations/bhadreshwar.html', 0.6, 'monthly'),
-        ('destinations/bhuj.html', 0.8, 'weekly'),
-        ('destinations/bhujodi.html', 0.7, 'monthly'),
-        ('destinations/dholavira.html', 0.8, 'weekly'),
-        ('destinations/dhordo-white-rann.html', 0.9, 'weekly'),
-        ('destinations/dhrang-mekan-dada.html', 0.6, 'monthly'),
-        ('destinations/gandhidham.html', 0.6, 'monthly'),
-        ('destinations/gangeshwar-mahadev.html', 0.6, 'monthly'),
-        ('destinations/haji-pir.html', 0.6, 'monthly'),
-        ('destinations/hanuman-tekri-kodki.html', 0.6, 'monthly'),
-        ('destinations/hiralaxmi-memorial-park.html', 0.6, 'monthly'),
-        ('destinations/jadura.html', 0.6, 'monthly'),
-        ('destinations/jakhau.html', 0.6, 'monthly'),
-        ('destinations/kadia-dhrow.html', 0.8, 'weekly'),
-        ('destinations/kalo-dungar.html', 0.7, 'monthly'),
-        ('destinations/kandla.html', 0.6, 'monthly'),
-        ('destinations/kashi-vishwanath-beach.html', 0.6, 'monthly'),
-        ('destinations/kotay-surya-mandir.html', 0.6, 'monthly'),
-        ('destinations/lakhpat.html', 0.7, 'monthly'),
-        ('destinations/madhapar.html', 0.6, 'monthly'),
-        ('destinations/mandvi.html', 0.8, 'weekly'),
-        ('destinations/mata-na-madh.html', 0.6, 'monthly'),
-        ('destinations/matang-matiya-dev.html', 0.6, 'monthly'),
-        ('destinations/mundra.html', 0.6, 'monthly'),
-        ('destinations/narayan-sarovar-koteshwar.html', 0.7, 'monthly'),
-        ('destinations/nirona.html', 0.7, 'monthly'),
-        ('destinations/road-to-heaven.html', 0.7, 'weekly'),
-        ('destinations/rudramata-dam.html', 0.6, 'monthly'),
-        
-        # Hidden gems
-        ('hidden-gems/chaari-dhand.html', 0.6, 'monthly'),
-    ]
+    site_dir = 'site'
+    pages = []
     
+    # Walk the site directory
+    for root, dirs, files in os.walk(site_dir):
+        # Exclude directories
+        if 'assets' in dirs:
+            dirs.remove('assets')
+        if 'destinations_backup' in dirs:
+            dirs.remove('destinations_backup')
+        # Also remove any other backup dirs just in case
+        dirs[:] = [d for d in dirs if not d.endswith('_backup')]
+            
+        for file in files:
+            if file.endswith('.html') and not file.startswith('_') and not file.startswith('google'):
+                # Get the relative path from 'site' directory
+                rel_path = os.path.relpath(os.path.join(root, file), site_dir)
+                
+                # Convert backslashes to forward slashes for URLs
+                url_path = rel_path.replace('\\', '/')
+                
+                # Assign default priorities and change frequencies
+                priority = 0.6
+                changefreq = 'monthly'
+                
+                if url_path == 'index.html':
+                    url_path = ''
+                    priority = 1.0
+                    changefreq = 'weekly'
+                elif url_path in ['about.html', 'blog.html', 'bookings.html', 'crafts.html', 'destinations.html', 'hidden-gems.html']:
+                    priority = 0.8
+                    changefreq = 'weekly'
+                elif url_path.startswith('destinations/'):
+                    # Some important destinations
+                    if file in ['bhuj.html', 'dholavira.html', 'dhordo-white-rann.html', 'mandvi.html']:
+                        priority = 0.8
+                        changefreq = 'weekly'
+                    else:
+                        priority = 0.7
+                        changefreq = 'monthly'
+                elif url_path.startswith('crafts/'):
+                    priority = 0.7
+                    changefreq = 'monthly'
+                
+                pages.append((url_path, priority, changefreq))
+    
+    # Sort pages to have empty (homepage) first, then alphabetical
+    pages.sort(key=lambda x: (x[0] != '', x[0]))
+
     # Create XML structure
     urlset = Element('urlset')
     urlset.set('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
